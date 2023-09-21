71dd369ef57d0dd6c438ab2b2045961e5e07b92d
= <=>
2023-09-21 19:26:10 -0300

    Init

---
+++ tercera_preentrega/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for tercera_preentrega project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tercera_preentrega.views import saludo, probando_template, probando_template2
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("saludo/", saludo),
+    path('probando_template/', probando_template),
+    path('probando_template2/', probando_template2)
+]
